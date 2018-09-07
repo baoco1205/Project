f98508f5784d66da1b4bbea761da4883e39fb1a3
Baoco15254516894 <co.letruongbao@students.hueic.edu.vn>
2018-09-07 15:09:04 +0700

    DanhSachSinhVien

---
+++ src/co/dev/MainApp.java
@@ -0,0 +1,21 @@
+package co.dev;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainApp {
+
+	public static void main(String[] args) throws IOException  {		
+			FileInputStream fis= new FileInputStream("in.txt");
+			BufferedReader  br = new BufferedReader(new InputStreamReader(fis));
+		    String line;
+		   while((line=br.readLine())!=null) {
+			   System.out.println(line);
+		      }
+		   br.close();
+	   }
+	}
+
+
